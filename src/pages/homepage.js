690df65170be3ca4264649e7bb149d9b6df361dd
ertugocer <101149653+ertugocer@users.noreply.github.com>
2024-01-16 23:18:42 +0300

    Gün 1

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,14 +3,15 @@ import {
     CardMedia,
     Container,
     Grid,
-    Link
+    Typography,
 } from '@mui/material'
+import Box from '@mui/material/Box';
 import '../images/home.jpg'
-//import Villagehomes from '../Component/Common/Villagehomes';
+import '../App.css';
 
 const home = () => {
     return (
-        <a>
+        <Grid className="ana">
         <Grid>
             <CardMedia
             component="img"
@@ -18,20 +19,42 @@ const home = () => {
             alt="home"
             />
         </Grid>
-        <Container>
-            <Grid container >
-                <Grid xs={12}>aaa</Grid>
-                <Grid xs={12}>aaa
+            <Grid container>
+                <Grid xs={12}><CardMedia
+            component="img"
+            image={require('../images/co.png')}
+            alt="home"
+            /></Grid>
+                <Grid xs={12}>
                     <Grid container>
-                        <Grid xs>3</Grid>
-                        <Grid xs>3</Grid>
-                        <Grid xs>3</Grid>
+                        <Grid item xs className="kim">
+                            <Box className="yazı2">
+                            Yılların verdiği tecrübe ve kalite ile emin ellerdesiniz. Uzman ekibimiz ile geliştirdiğmiz projelerde her zaman en yeni sistemleri
+                            kullanarak bu sektörde göz önünde olmayı başardık. TechSlot şirketi kurulduğundan bu yana müşterilerine çözüm odaklı olarak
+                            yeni nesil yazılım ve danışmanlık hizmeti vermekte.
+                            </Box>
+</Grid>
+                        <Grid item xs className="cozum">
+                            <Box className="yazı1">
+                            Çözümler için oluşturulmuş kodlamalar
+                            </Box>
+                        </Grid>
+                        <Grid item xs className="kim">
+                        <Box className="yazı3">
+                            
+                            </Box>
+                            </Grid>
                     </Grid>
                 </Grid>
-                <Grid xs={12}>aaa</Grid>
+                <Grid xs={12}>
+                    <CardMedia
+            component="img"
+            image={require('../images/co.png')}
+            alt="home"
+            />
             </Grid>
-        </Container>
-        </a>
+            </Grid>
+        </Grid>
     )
 
 }
